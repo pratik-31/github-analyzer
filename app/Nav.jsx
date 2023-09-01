7bd2402fec45c5e9ea7490f913d639ebcad47819
Pratik Agarwal <pratikkumar.agarwal123@gmail.com>
2023-09-02 00:30:54 +0530

    minor fix

--- app/Nav.jsx
+++ app/Nav.jsx
@@ -59,7 +59,10 @@ const Nav = () => {
                 <div className="relative mr-2">
                   <button
                     type="submit"
-                    onClick={() => signIn({ callbackUrl: "/" })}
+                    onClick={() => {
+                      signIn({ callbackUrl: "/" });
+                      window.location.reload();
+                    }}
                     className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br bg-black hover:text-white  focus:ring-4 focus:outline-none focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                   >
                     <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white text-gray-900 hover:text-gray-50  rounded-md group-hover:bg-opacity-0">
